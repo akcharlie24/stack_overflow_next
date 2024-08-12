1c7ad23d8a6704a165cf4c92463f7e5b83ca3973
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-08-12 09:45:10 +0530

    prod fix

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]); // (.*) this is to catch all the routes after sign-in and sign-up
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/",
+  "/api/webhooks(.*)",
+  "/api/chatgpt",
+  "/question/:id",
+  "/tags",
+  "/tags/:id",
+  "/profile/:id",
+  "/community",
+  "/jobs",
+]); // (.*) this is to catch all the routes after sign-in and sign-up
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
