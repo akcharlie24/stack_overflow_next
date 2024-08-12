04a47a86e6af49f97baa59c9a8d7c0becf4e5286
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-08-12 15:06:10 +0530

    prod fix

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
+  // TODO: Check the signUp bug (possibly due to webhook/clerk issue/middleware)
   return <SignUp />;
 }
