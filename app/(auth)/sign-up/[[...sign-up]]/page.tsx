586a760a60c167cbf8edadc9dc568bc36ef631c2
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-08-12 15:14:46 +0530

    prod fix

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,5 +2,5 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
   // TODO: Check the signUp bug (possibly due to webhook/clerk issue/middleware)
-  return <SignUp />;
+  return <SignUp forceRedirectUrl={"/"} />;
 }
