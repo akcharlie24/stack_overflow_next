a2fa4cb225b76d8032f4d16c805c9703f69a16d9
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-08-12 08:44:25 +0530

    prod ready to test webhooks

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -5,95 +5,14 @@ import NoResult from "@/components/shared/NoResult";
 import LocalSearch from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
+import { getQuestions } from "@/lib/actions/question.action";
 import Link from "next/link";
 import React from "react";
 
-const questions = [
-  {
-    _id: 1,
-    title: "What is the difference between React and Vue.js?",
-    tags: [
-      { _id: "1", name: "javascript" },
-      { _id: "2", name: "frontend" },
-    ],
-    author: {
-      _id: 123,
-      name: "John Doe",
-      picture: "https://example.com/profile_pictures/john_doe.jpg", // Replace with actual image URL
-    },
-    upvotes: 250000,
-    views: 10000000,
-    answers: [
-      {
-        _id: 456,
-        content:
-          "Both are popular JavaScript libraries for building user interfaces. React uses a virtual DOM for efficient updates, while Vue.js offers a more lightweight approach.",
-        author: {
-          _id: 45,
-          name: "Jane Smith",
-          picture: "https://example.com/profile_pictures/jane_smith.jpg", // Replace with actual image URL
-        },
-        createdAt: new Date("2024-06-02"),
-      },
-    ],
-    createdAt: new Date("2024-05-30"),
-  },
-  {
-    _id: 2,
-    title: "How do I implement routing in a React application?",
-    tags: [
-      { _id: "3", name: "react" },
-      { _id: "4", name: "routing" },
-    ],
-    author: {
-      _id: 456,
-      name: "Jane Smith",
-      picture: "https://example.com/profile_pictures/jane_smith.jpg", // Replace with actual image URL
-    },
-    upvotes: 18,
-    views: 750,
-    answers: [], // Empty answers array
-    createdAt: new Date("2024-06-01"),
-  },
-  {
-    _id: 3,
-    title: "What are the best practices for state management in React?",
-    tags: [
-      { _id: "1", name: "javascript" },
-      { _id: "5", name: "react" },
-      { _id: "6", name: "state" },
-    ],
-    author: {
-      _id: 789,
-      name: "Michael Jones",
-      picture: "https://example.com/profile_pictures/michael_jones.jpg", // Replace with actual image URL
-    },
-    upvotes: 32,
-    views: 1500,
-    answers: [], // Empty answers array
-    createdAt: new Date("2024-05-28"),
-  },
-  {
-    _id: 4,
-    title: "Explain the concept of functional components in React.",
-    tags: [
-      { _id: "1", name: "javascript" },
-      { _id: "5", name: "react" },
-      { _id: "7", name: "components" },
-    ],
-    author: {
-      _id: 1011,
-      name: "Sarah Lee",
-      picture: "https://example.com/profile_pictures/sarah_lee.jpg", // Replace with actual image URL
-    },
-    upvotes: 12,
-    views: 400,
-    answers: [], // Empty answers array
-    createdAt: new Date("2024-05-25"),
-  },
-];
+const Home = async () => {
+  const result = await getQuestions({});
+  console.log(result.questions);
 
-const Home = () => {
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -120,8 +39,8 @@ const Home = () => {
       </div>
       <HomeFilters />
       <div className="mt-10 flex w-full flex-col gap-6">
-        {questions.length > 0 ? (
-          questions.map((question) => (
+        {result.questions.length > 0 ? (
+          result.questions.map((question) => (
             <QuestionCard
               key={question._id}
               _id={question._id}
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
   try {
@@ -17,3 +24,67 @@ export async function getUserById(params: any) {
     throw error;
   }
 }
+
+export const createUser = async (userData: CreateUserParams) => {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const updateUser = async (params: UpdateUserParams) => {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    // good practice to revalidate after server action
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const deleteUser = async (params: DeleteUserParams) => {
+  try {
+    connectToDatabase();
+
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    // if user exist delete user data from database
+    // quesions,answers,comments etc
+
+    // get user question ids
+
+    // const userQuestionIds = await Question.find({
+    //   author: user._id,
+    // }).distinct("_id");
+
+    // delete user Questions
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments etc
+
+    // delete user
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    return deletedUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
