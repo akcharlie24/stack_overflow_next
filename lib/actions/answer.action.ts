090901ebae4a65b8bbd5a1c7ca0b24e3a9f006f2
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-08-15 14:45:30 +0530

    done with answer and collectino

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -2,7 +2,11 @@
 
 import Answer from "@/database/answer.model";
 import { connectToDatabase } from "../mongoose";
-import { CreateAnswerParams, GetAnswersParams } from "./shared.types";
+import {
+  AnswerVoteParams,
+  CreateAnswerParams,
+  GetAnswersParams,
+} from "./shared.types";
 import Question from "@/database/question.model";
 import { revalidatePath } from "next/cache";
 
@@ -37,6 +41,73 @@ export const getAnswers = async (params: GetAnswersParams) => {
       .sort({ createdAt: -1 });
 
     return { answers };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const upvoteAnswer = async (params: AnswerVoteParams) => {
+  try {
+    connectToDatabase();
+
+    const { answerId, userId, hasupVoted, path, hasdownVoted } = params;
+
+    let updateQuery = {};
+    if (hasupVoted) {
+      updateQuery = { $pull: { upvotes: userId } };
+    } else if (hasdownVoted) {
+      updateQuery = {
+        $pull: { downvotes: userId },
+        $push: { upvotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { upvotes: userId } };
+    }
+
+    const answer = await Answer.findByIdAndUpdate(answerId, updateQuery, {
+      new: true,
+    });
+    if (!answer) {
+      throw new Error("No Answer found");
+    }
+
+    //  increment author's reputation +2 points for upvoting an answer
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const downvoteAnswer = async (params: AnswerVoteParams) => {
+  try {
+    connectToDatabase();
+
+    const { answerId, userId, hasupVoted, path, hasdownVoted } = params;
+
+    let updateQuery = {};
+    if (hasdownVoted) {
+      updateQuery = { $pull: { downvotes: userId } };
+    } else if (hasupVoted) {
+      updateQuery = {
+        $pull: { upvotes: userId },
+        $push: { downvotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { downvotes: userId } };
+    }
+
+    const answer = await Answer.findByIdAndUpdate(answerId, updateQuery, {
+      new: true,
+    });
+    if (!answer) {
+      throw new Error("No answer found");
+    }
+
+    //  decrease author's reputation
+    revalidatePath(path);
   } catch (error) {
     console.log(error);
     throw error;
